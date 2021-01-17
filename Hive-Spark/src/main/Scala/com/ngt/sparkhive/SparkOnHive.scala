c8ba07307e8a429704708b77e7ba3bc9ea36c437
kailanyue <yuekailan@163.com>
2021-01-17 22:07:05 +0800

    v0.2

--- Hive-Spark/src/main/Scala/com/ngt/sparkhive/SparkOnHive.scala
+++ Hive-Spark/src/main/Scala/com/ngt/sparkhive/SparkOnHive.scala
@@ -12,4 +12,26 @@ case class SparkOnHive() {
     .enableHiveSupport()
     .config("spark.driver.host", "lx")
     .getOrCreate()
+
+  @Test //ok
+  def testLocal() {
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("AppLocal2")
+      .config("spark.driver.host", "lx") // 多网卡机器，会识别到错误的主机名，要写绝对地址，不要 localhost
+      .enableHiveSupport()
+      .getOrCreate()
+
+    //spark.sparkContext.setLogLevel("warn")
+    import spark.sql
+
+    //hdfs 启用 LZO 压缩以后，需要在客户端添加 LZO 依赖库的环境变量和 maven 坐标，windows本地配置lzo读取所需相关组件_运维_coolerzZ的博客-CSDN博客 https://blog.csdn.net/coolerzz/article/details/103952188
+    sql(
+      """
+				|select * from szt.ods_szt_data limit 100
+				|""".stripMargin)
+      .show(200, false)
+
+    spark.stop()
+  }
 }
